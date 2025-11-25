df8b9ec3cac36374f37271cc6a3341d67adbd469
alfonzoregaspi <alfonzo_regaspi@dlsu.edu.ph>
2025-11-26 00:28:12 +0800

    Added comments
    Added comments to clarify the purpose of components.

--- ReceiptPanel.java
+++ ReceiptPanel.java
@@ -4,13 +4,13 @@ import java.awt.*;
 public class ReceiptPanel extends JPanel {
 
     public ReceiptPanel(StoreGUI frame, Receipt receipt, double change) {
-        setLayout(new BorderLayout());
-
+        setLayout(new BorderLayout()); //Uses BorderLayout to organize components
+        //Text area to show the generated receipt text
         JTextArea area = new JTextArea();
-        area.setEditable(false);
+        area.setEditable(false); //User cannot edit/modify the receipt
         area.setFont(new Font("Monospaced", Font.PLAIN, 12));
         area.setText(receipt.getFormattedReceipt() + String.format("\nChange: ₱%.2f\n", change));
-
+        //Button for saving the receipt to a txt file
         JButton printBtn = new JButton("Save Receipt to File");
         printBtn.addActionListener(e -> {
             receipt.saveToFile();
@@ -24,7 +24,8 @@ public class ReceiptPanel extends JPanel {
         bottom.add(printBtn);
         bottom.add(newTransBtn);
 
-        add(new JScrollPane(area), BorderLayout.CENTER);
-        add(bottom, BorderLayout.SOUTH);
+        add(new JScrollPane(area), BorderLayout.CENTER); //scrollable receipt display    
+        add(bottom, BorderLayout.SOUTH); //buttons at the bottom
     }
 }
+
